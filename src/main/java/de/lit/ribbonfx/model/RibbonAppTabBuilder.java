bf455b974e3cc023bcab8e6e210766c161a99388
aliebelt <andreas.liebelt@iwes.fraunhofer.de>
2014-01-13 20:02:41 +0100

    App Tab Builder added

---
+++ src/main/java/de/lit/ribbonfx/model/RibbonAppTabBuilder.java
@@ -0,0 +1,30 @@
+package de.lit.ribbonfx.model;
+
+/**
+ * @author aliebelt
+ */
+public class RibbonAppTabBuilder {
+
+	RibbonAppTab ribbonAppTab;
+
+	/**
+	 * Hidden constructor
+	 */
+	RibbonAppTabBuilder() {
+		this.ribbonAppTab = new RibbonAppTab();
+	}
+
+	public static RibbonAppTabBuilder create() {
+		return new RibbonAppTabBuilder();
+	}
+
+	public RibbonAppTabBuilder title(String title) {
+		this.ribbonAppTab.title().set(title);
+		return this;
+	}
+
+	public RibbonAppTab build() {
+		return this.ribbonAppTab;
+	}
+
+}
