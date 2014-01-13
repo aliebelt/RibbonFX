85c7f67ecb6b3fb5abb7398ee003bc806e5885aa
aliebelt <andreas.liebelt@iwes.fraunhofer.de>
2014-01-13 22:58:44 +0100

    ribbon tab area styling

--- src/test/java/de/lit/ribbonfx/RibbonRunTest.java
+++ src/test/java/de/lit/ribbonfx/RibbonRunTest.java
@@ -11,8 +11,9 @@ public class RibbonRunTest extends Application {
 
 	@Override
 	public void start(Stage primaryStage) throws Exception {
-		RibbonTab ribbonTab = RibbonTabBuilder.create().title("First Tab").build();
-		TabPane ribbon = RibbonBuilder.create().tabs(ribbonTab).build();
+		RibbonTab ribbonTab1 = RibbonTabBuilder.create().title("Start").build();
+		RibbonTab ribbonTab2 = RibbonTabBuilder.create().title("Einfügen").build();
+		TabPane ribbon = RibbonBuilder.create().tabs(ribbonTab1, ribbonTab2).build();
 
 		primaryStage.setScene(new Scene(ribbon, 650, 350, Color.TRANSPARENT));
 		primaryStage.initStyle(StageStyle.UNIFIED);
