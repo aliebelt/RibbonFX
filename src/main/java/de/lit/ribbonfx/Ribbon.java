d402f6921a0bd89b625ffc1bfb69fe0b5791afbc
aliebelt <andreas.liebelt@iwes.fraunhofer.de>
2014-01-30 09:37:15 +0100

    ribbon container anchor pane

--- src/main/java/de/lit/ribbonfx/Ribbon.java
+++ src/main/java/de/lit/ribbonfx/Ribbon.java
@@ -5,15 +5,15 @@ import javafx.beans.property.ObjectPropertyBase;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.control.TabPane;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.Background;
 import javafx.scene.layout.BackgroundFill;
-import javafx.scene.layout.Region;
 import javafx.scene.paint.Paint;
 import de.lit.ribbonfx.actions.ButtonAction;
 import de.lit.ribbonfx.presentation.container.ContainerPresenter;
 import de.lit.ribbonfx.presentation.container.ContainerView;
 
-public class Ribbon extends Region {
+public class Ribbon extends AnchorPane {
 
 	TabPane tabPane;
 	ContainerPresenter tabPanePresenter;
@@ -28,8 +28,10 @@ public class Ribbon extends Region {
 		this.tabPanePresenter = (ContainerPresenter) tabPaneView.getPresenter();
 
 		getChildren().add(this.tabPane);
-		this.tabPane.prefWidthProperty().bind(widthProperty());
-		this.tabPane.prefHeightProperty().bind(heightProperty());
+		AnchorPane.setBottomAnchor(this.tabPane, 0.0);
+		AnchorPane.setLeftAnchor(this.tabPane, 0.0);
+		AnchorPane.setRightAnchor(this.tabPane, 0.0);
+		AnchorPane.setTopAnchor(this.tabPane, 0.0);
 	}
 
 	public void setAccent(Paint accent) {
