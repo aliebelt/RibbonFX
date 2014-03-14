b22cd442ade077417fefa9eefa62244f05da258a
aliebelt <andreas.liebelt@iwes.fraunhofer.de>
2014-03-14 11:04:07 +0100

    footer resize corner switch

--- src/main/java/de/lit/ribbonfx/presentation/footer/FooterPresenter.java
+++ src/main/java/de/lit/ribbonfx/presentation/footer/FooterPresenter.java
@@ -6,13 +6,20 @@ import java.util.ResourceBundle;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.HBox;
+import javafx.stage.Stage;
+import javafx.stage.Window;
 
 public class FooterPresenter implements Initializable {
 
+	static final double FOOTER_RIGHT_CONTENT_OFFSET = 15d;
+
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
-		// Empty
+		initStageStateListening();
 	}
 
 	public void addLeftButton(Parent button) {
@@ -23,10 +30,51 @@ public class FooterPresenter implements Initializable {
 		this.hBoxRightButtons.getChildren().add(button);
 	}
 
+	public void enableCornerResize(boolean enable) {
+		imageViewCornerResize.setVisible(enable);
+		if (enable) {
+			AnchorPane.setRightAnchor(hBoxRightButtons, FOOTER_RIGHT_CONTENT_OFFSET);
+		} else {
+			AnchorPane.setRightAnchor(hBoxRightButtons, 0d);
+		}
+	}
+
+	private void initStageStateListening() {
+		processScene(hBoxLeftButtons.getScene());
+		hBoxLeftButtons.sceneProperty().addListener((sceneProperty, oldScene, newScene) -> {
+			if (newScene != null) {
+				processScene(newScene);
+				newScene.windowProperty().addListener((windowProperty, oldWindow, newWindow) -> {
+					processStage(newWindow);
+				});
+			}
+		});
+	}
+
+	private void processScene(Scene scene) {
+		if (scene != null) {
+			processStage(scene.getWindow());
+		}
+	}
+
+	private Stage processStage(Window window) {
+		if (window != null && window instanceof Stage) {
+			Stage stage = (Stage) window;
+			enableCornerResize(!(stage.isMaximized() || stage.isFullScreen()));
+			stage.maximizedProperty().addListener(o -> enableCornerResize(!stage.isMaximized()));
+			stage.fullScreenProperty().addListener(o -> enableCornerResize(!stage.isFullScreen()));
+			return stage;
+		}
+		return null;
+	}
+
 	@FXML
 	HBox hBoxLeftButtons;
 
 	@FXML
 	HBox hBoxRightButtons;
+
+	@FXML
+	ImageView imageViewCornerResize;
 
 }
